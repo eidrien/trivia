9fe06d0dbd9150232e14a41cb013725f99dfed22
Adrián Perreau de Pinninck Bas <aperreau@gmail.com>
2012-04-22 19:30:46 +0200

    Added test with a fake OutputStream to test the original output. Not passing since there is a random number generator which is not controlled yet.

--- test/com/adaptionsoft/games/trivia/runner/GameRunnerTest.java
+++ test/com/adaptionsoft/games/trivia/runner/GameRunnerTest.java
@@ -1,6 +1,32 @@
 package com.adaptionsoft.games.trivia.runner;
 
+import static org.junit.Assert.*;
 
-public class GameRunnerTest {
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
+import org.junit.Test;
+
+
+public class GameRunnerTest{
+
+	@Test
+	public void runWithoutChanging(){
+		OutputStreamAccumulator outputStreamAccumulator = new OutputStreamAccumulator();
+		System.setOut(new PrintStream(outputStreamAccumulator)); 
+		GameRunner.main(null);
+		assertEquals(0, outputStreamAccumulator.accumulatedValue);
+	}
+	
+	class OutputStreamAccumulator extends OutputStream {
+
+		int accumulatedValue;
+		
+		@Override
+		public void write(int value) throws IOException {
+			accumulatedValue += value;
+		}
+		
+	}
 }
